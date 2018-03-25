ab02113aac16f872d8959e51243901bd0132ecff
bkozak <bartek.kozak17@gmail.com>
2018-03-25 22:06:43 +0200

    Add project files.

---
+++ MyjniaWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyjniaWebApp.Startup))]
+namespace MyjniaWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
